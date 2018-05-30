f63cf23b0c86698a7b8de8cbb9e371c8494ed6a1
tchabaud <tom@chabs.name>
2018-05-30 22:46:00 +0200

    Switch theme to use Quark

--- themes/koulenga/koulenga.php
+++ themes/koulenga/koulenga.php
@@ -1,7 +1,9 @@
 <?php
-    namespace Grav\Theme;
-    class Koulenga extends Antimatter
-    {
-       // Some new methods, properties etc.
-    }
+// Default theme definition
+namespace Grav\Theme;
+// Inherits from default theme class
+class Koulenga extends Quark
+{
+    // Some new methods, properties etc.
+}
 ?>
