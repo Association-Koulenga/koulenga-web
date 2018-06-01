b25bce023e5f589e9618092a29b1a1c9e052310c
tchabaud <tom@chabs.name>
2018-06-01 22:00:34 +0200

    Fix php linter warnings

--- themes/koulenga/koulenga.php
+++ themes/koulenga/koulenga.php
@@ -1,7 +1,26 @@
 <?php
-// Default theme definition
+/**
+ * Default theme definition.
+ * 
+ * PHP version 7
+ * 
+ * @category Koulenga
+ * @package  Grav\Theme
+ * @author   tchabaud <dev@chabs.name>
+ * @license  GPL3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link     http://koulenga.com
+ */
 namespace Grav\Theme;
-// Inherits from default theme class
+
+/**
+ * Extends default theme main class.
+ * 
+ * @category Koulenga
+ * @package  Grav\Theme
+ * @author   tchabaud <dev@chabs.name>
+ * @license  GPL3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link     http://koulenga.com
+ */
 class Koulenga extends Quark
 {
     // Some new methods, properties etc.
