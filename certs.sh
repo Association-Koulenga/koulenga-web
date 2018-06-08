af4506dff47dca469696a74e21cf5ca4633325da
tchabaud <tom@chabs.name>
2018-06-08 23:11:36 +0200

    Raise validity delay. Add some variables.

--- certs.sh
+++ certs.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-CERTS_DIR="${PWD}/certs"
-KEY_SIZE=4096
+set -x
 
-openssl req -x509 -out "${CERTS_DIR}"/localhost.crt -keyout "${CERTS_DIR}"/localhost.key \
-  -newkey rsa:${KEY_SIZE} -nodes -sha256 \
-  -subj '/CN=localhost' -extensions EXT -config <(printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+CERTS_DIR="${PWD}/certs"
+KEY_SIZE="4096"
+DOMAIN="localhost"
+CERT="${DOMAIN}.crt"
+KEY="${DOMAIN}.key"
+
+openssl req -x509 -days 3650 -out "${CERTS_DIR}/${CERT}" -keyout "${CERTS_DIR}/${KEY}" \
+  -newkey rsa:"${KEY_SIZE}" -nodes -sha256 \
+  -subj '/CN='"${DOMAIN}" -extensions EXT -config <(printf "[dn]\nCN=%s\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:%s\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth" "${DOMAIN}" "${DOMAIN}")
